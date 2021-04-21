cae57dbc310f93c32eaef51d5a9a3b8faddcae75
George Loera <gloera8325@gmail.com>
2021-04-21 09:34:27 -0500

    created a genre attribute

--- music_project/music/models.py
+++ music_project/music/models.py
@@ -7,4 +7,5 @@ class Song(models.Model):
     title = models.CharField(max_length=50)
     artist = models.CharField(max_length=50)
     album = models.CharField(max_length=50)
+    genre = models.CharField(default=None, max_length=50)
     release_date = models.DateTimeField()
